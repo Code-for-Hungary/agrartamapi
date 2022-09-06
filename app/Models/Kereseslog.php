0d654e147727c522352cb673878df5cf0a450fcb
law909 <balint.lovey@gmail.com>
2022-09-07 01:44:53 +0200

    no more debug log

--- app/Models/Kereseslog.php
+++ app/Models/Kereseslog.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 /**
  * App\Models\Kereseslog
