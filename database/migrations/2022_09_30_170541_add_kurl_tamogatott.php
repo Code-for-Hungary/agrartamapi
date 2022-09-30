05f967be1a77266cf31fec61198af7e34c79a487
law909 <balint.lovey@gmail.com>
2022-09-30 19:35:45 +0200

    drop column

--- database/migrations/2022_09_30_170541_add_kurl_tamogatott.php
+++ database/migrations/2022_09_30_170541_add_kurl_tamogatott.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('tamogatotts', function (Blueprint $table) {
-            $table->removeColumn('kurl');
+            $table->dropColumn('kurl');
         });
     }
 };
